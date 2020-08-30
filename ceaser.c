0c299a1c0c31bb298409c5d9b831f396847c3fb9
Anna Bakas <abakas@luc.edu>
2020-08-30 11:46:36 -0700

    Open and print encrypted_text.txt

--- ceaser.c
+++ ceaser.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 	int main(){
+
+		/*opens file*/
+		FILE *fp;
+		fp = fopen("encrypted_text.txt", "r");
+
+		char c;
+		
+		/*checks to see if file was successfully opened*/
+
+		if (fp == NULL){
+		       	printf("Error while opening file");
+		}
+		
+		/*prints output of text file*/
+
+		do{ 
+			c = fgetc(fp);
+			putchar(c);
+		}while(c!=EOF);
+
+		fclose(fp);
+
 		return 0;
+		
 	}
 
