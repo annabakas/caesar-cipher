6a003aa062e58ff9affff128d3656e83cb6f7151
Anna Bakas <abakas@luc.edu>
2020-09-16 09:32:22 -0500

    Working on openDict() and buffer

--- ceaser.c
+++ ceaser.c
@@ -4,10 +4,10 @@
 
 #define SHIFTS 25
 
-const int INITIAL_MAX_LINES = 2;
-const int MAX_LINES_INC = 2;
-const int INITIAL_MAX_LINE_LENGTH = 2;
-const int MAX_LINE_LENGTH_INC = 2;
+const int INITIAL_MAX_LINES = 50;
+const int MAX_LINES_INC = 50;
+const int INITIAL_MAX_LINE_LENGTH = 50;
+const int MAX_LINE_LENGTH_INC = 50;
 
 char shifts[SHIFTS];
 int max_index;
@@ -25,7 +25,7 @@ int max_index;
 		int maxLines, c, buflen, bufp, endline;
 
 		maxLines = INITIAL_MAX_LINES;
-		
+		max = shifts[0];
 		//Allocating memory for inputFile
 		file = (char **)malloc(maxLines*sizeof(char*));
 
@@ -43,7 +43,7 @@ int max_index;
 			c = 0;
 
 			while(c != EOF){
-				
+
 				endline = 0;
 
 				//Grabbing characters
@@ -53,7 +53,7 @@ int max_index;
 				if(c==EOF || c=='\n' || c=='\r'){
 					//Setting end of line to true/1
 					endline = 1;
-				
+
 				}
 				//Putting character in buffer
 				//Check if there is enough memory
@@ -61,88 +61,88 @@ int max_index;
 				//Incrementing buffer position
 				else{
 					if(bufp >= buflen - 1){
-						//fprintf(stderr, "reallocation buffer\n");
 						buflen+=MAX_LINE_LENGTH_INC;
 						buffer = (char *) realloc(buffer, buflen * sizeof(char));
 					}
-					
+
 					buffer[bufp] = c;
 					bufp++;
 				}
-				
+
 				//If end of line was reached, get a new buffer
 				//Check if more memory is needed
 				//Realloc memory to inputFile
 				if(endline){
 					if(lines >= maxLines){
-						//fprintf(stderr, "reallocation lines\n");
 						maxLines += MAX_LINES_INC;
 						file = (char**) realloc(file, maxLines * sizeof(char*));
 					}
 					//Null terminate buffer
 					buffer[bufp++] = 0;
-					
+				
 					//Add buffer to inputFile
 					//Increment line
 					file[lines] = buffer;
+					//printf("%d: %s\n", lines,file[lines]);
+					result = strcmp(file[lines], decrypted);
+					if(result == 0){
+						printf("match\n");
+						shifts[key]++;
+						if(shifts[key] > max){
+							max_index = key;
+						}
+					}
 					lines++;
 
 					//Resets buffer
 					bufp = 0;
 					buflen = INITIAL_MAX_LINE_LENGTH;
-					//free(buffer);
-					//buffer = (char*) malloc(buflen * sizeof(char));
-					buffer[0] = '\0';
+					//buffer = (char*) malloc(buflen * sizeof(char*));
+					buffer[0] = 0;
 				}
 			}
 		}
-		
+
 		//printf("Key: %d %s\n",key, decrypted);
 		
 		//printf("%d lines\n", nlines);
-		max = shifts[0];
+		//max = shifts[0];
+		/*
 		//Go through dictionary and compare dictionary word to decrypted word
 		//Increment found if result == 0
 		for(int i=0; i<lines;i++){
-			//printf("%s\n", file[i]);
+			printf("%s\n", file[i]);
 			result = strcmp(file[i], decrypted);
-			//printf("%d\n", result);
+			
 			if(result == 0){
-				//printf("Found %d\n", result);
-				//found++;	
+				//printf("Found\n");
+				found++;
 				//return 1;
-				shifts[key]+=1;
-				printf("%d\n", shifts[key]);
+				shifts[key] += 1;
 				if(shifts[key] > max){
 					max_index = key;
 				}
-				
-						
 			}
 			else{
-				//return 0;
+				//printf("not found\n");
 			}
-			
-		}
-		
-		//printf("Dictionary: %d\n", found);
-		
+
+		}*/
+
 		free(file[0]);
-		
 		fclose(fp);
-		return 0;
 	}
 
 	//Decrypts word(s) from sentences
 	//Passes decrypted word and key/shift to openDict() to check 
 	//if it's in the dictionary
 	char* decrypt(char *word){
-		//printf("\n%s\n", word);
+		printf("\n%s\n", word);
 		char ch;
 		
 		char *decrypted;
 		decrypted = (char*) calloc(1,sizeof(char));
-		
+		//char decrypted[150];
 		for(int key = 1; key < 26; key++){
 			for(int i=0; word[i] != '\0'; ++i){
 				int k = 0;
@@ -164,7 +164,7 @@ int max_index;
 			openDict(decrypted, key);
 		}
 		
-		free(decrypted);
+		//free(decrypted);
 	}
 
 	//Takes in sentence from encrypted_text
@@ -176,27 +176,19 @@ int max_index;
 		int tempCounter = 0;
 		
 		char *token = strtok(l, " ");
-		
+		token = strtok(NULL, " ");
 		while(token != NULL)
 		{
 			//NULL passed as first argument in order to
 			//continue with the same string (line)
-			if(tempCounter == 0){
-				token = strtok(NULL, " ");
-				tempCounter++;
-			}
-			//grows **words with realloc
-			//allocates size of each element of words using malloc
-			//copies token to words[counter] as counter is incremented
-			else{
+				//token = strtok(NULL, " ");
 				words = realloc(words, (counter+ 1) * sizeof(char *));
 				words[counter] = malloc(strlen(token) + 1);
 				decrypt(strcpy(words[counter++], token));
 				token = strtok(NULL, " ");
-			}
+		}
 			
 			counter = 0;
-		}
 	}	
 
 
@@ -209,21 +201,25 @@ int max_index;
 		
 		FILE *fp;
 		fp = fopen("shifts.txt", "w");
-
+		//openDict();
+		int l = 0;
 		while((read = getline(&line, &len, stdin)) <= EOF)
 		{
-			split(line);
 			
+			split(line);
+			line++;
 			//Writes best shift to shifts.txt
 			fprintf(fp, "%d\n", max_index);
 			
 			printf("\nBest Shift: %d\n", max_index);
+			
 			
 			//Resets occurences of each shift for each sentence
 			for(int i = 1; i<26; i++){
 				shifts[i] = 0;
 			}
 			//break;
+		
 		}
 
 		fclose(fp);
